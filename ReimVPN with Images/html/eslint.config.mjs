83956350c843347c84bab2ba56683bea6c89bf80
Fuzail Bukhari <bukharifuxail@gmail.com>
2025-07-26 16:51:55 +0500

    fix: reorganize ESLint configuration for improved clarity and structure

--- ReimVPN with Images/html/eslint.config.mjs
+++ ReimVPN with Images/html/eslint.config.mjs
@@ -5,15 +5,18 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
     languageOptions: {
       globals: {
+        ...globals.browser,
         AOS: "readonly",
         Swiper: "readonly",
       },
     },
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
   },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "script" },
+  },
 ]);
